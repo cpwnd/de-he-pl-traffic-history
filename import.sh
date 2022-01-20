7ab6e2dbe33764ea61e6bd2c97db72ca2d44474f
Chris <chris_pw@gmx.de>
2022-01-20 09:01:45 +0100

    Fix list type of eventCategory

--- import.sh
+++ import.sh
@@ -3,19 +3,24 @@
 git-history file de-he-pl-traffic.db traffic.json \
   --id id \
   --id dataId \
-  --convert 'data = json.loads(content)
+  --convert 'from functools import reduce
+data = json.loads(content)
 if data.get("status") != "OK":
     # {"code": 500, "error": "Error accessing remote data..."}
     return
 for item in data["data"]:
-    res = dict()
-    res["id"] = item["ticId"]["value"]
-    res["producer"] = item["dataProducer"]["value"]
-    res["dataId"] = item["dataIdentifier"]["value"]
-    res["verbal"] = item["description"][0]["value"]
-    res["urgency"] = item["urgency"]["value"]
-    res["eventCategory"] = item["eventCategory"][0]["value"]
-    res["begin"] = item["objectVersionBeginTime"]["value"]
-    res["status"] = item["objectVersionStatus"]["value"]
-    yield res
+    try:
+        res = dict()
+        res["id"] = item["ticId"]["value"]
+        res["producer"] = item["dataProducer"]["value"]
+        res["dataId"] = item["dataIdentifier"]["value"]
+        res["verbal"] = item["description"][0]["value"]
+        res["urgency"] = item["urgency"]["value"]
+        res["eventCategory"] = reduce(lambda a, b: a + "," + b, sorted(list(map(lambda x: x["value"], item["eventCategory"]))))
+        res["begin"] = item["objectVersionBeginTime"]["value"]
+        res["status"] = item["objectVersionStatus"]["value"]
+        yield res
+    except TypeError as e:
+        print(e)
+        continue
 '
